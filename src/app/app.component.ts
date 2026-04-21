ce1003394d374298ae9fbe90ef5f1fd27fedfef3
DavidJohnPerkins <DavidJohnPerkins@users.noreply.github.com>
2026-04-21 23:37:02 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DUMMY_USERS } from './dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+  //selectedId: string = DUMMY_USERS[0].id
+  selectedId?: string;
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedId)!;
+  }
+  onSelectUser(id: string) {
+    this.selectedId = id;
+    console.log('Selected user with id:' + id);
+  }
+}
